8394db6a801ea4a440a4b828ac3f8a7e872ca107
Bryan Jones <daedeross@gmail.com>
2017-07-16 15:00:39 -0500

    Initial commit upstream

---
+++ SCMAngular/ClientApp/app/app.module.shared.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './components/app/app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+
+export const sharedConfig: NgModule = {
+    bootstrap: [ AppComponent ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        HeaderComponent
+    ],
+    imports: [
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+            { path: '**', redirectTo: 'home' }
+        ])
+    ]
+};
